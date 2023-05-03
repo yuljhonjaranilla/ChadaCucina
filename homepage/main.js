e058dd53765f4c7307af4b116bde6660e190f55d
dridexmalware <yuljhonjaranilla@gmail.com>
2023-05-03 11:11:32 +0800

    May 03 - Draft

---
+++ homepage/main.js
@@ -0,0 +1,30 @@
+const navMenu = document.getElementById('nav_menu'),
+    navToggle = document.getElementById('nav_toggle'),
+    navClose = document.getElementById('nav_close')
+
+    if(navToggle){
+        navToggle.addEventListener('click', () => {
+            navMenu.classList.add('show-menu')
+        })
+    }
+
+    if(navClose){
+        navClose.addEventListener('click', () =>{
+            navMenu.classList.remove('show-menu')
+        })
+    }
+
+const navLink = document.querySelectorAll('.nav-link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav_menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+const scrollHeader = () => {
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('bg-header')
+                       : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', scrollHeader)
